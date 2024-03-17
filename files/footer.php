abe23272da5aa8c571e3b0ee42c21657ec5ebfaa
Sultanomar0013 <Sultanomar0013>
2024-03-18 05:19:12 +0600

    finalize design of this website

---
+++ files/footer.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>
+
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="../css/footer.css">
+    </head>
+    <body>
+
+        <div class="footer-main-div">
+            <div class="footer-sub-div"> 
+                        <div class="footer-title">
+                        <h5 class="footer-h3" >BOOK A MEETING WITH US!</h5>
+                        <button type="button" class="btn btn-light footer-btn"><strong>JOIN US</strong></button>
+                        </div>
+                    
+            </div>
+        </div>
+
+    
+    </body>
+</html>
+
+
+
+
